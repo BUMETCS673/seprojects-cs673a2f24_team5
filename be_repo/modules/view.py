46dc214e9e77736f16b95842330401a6f84fbfd5
zihan zhou <95243748+andyasdd1@users.noreply.github.com>
2024-11-30 23:11:34 -0500

    Add New Modules: GraphCypherQAChain. Add New Preprocess modules: embedding and nodes retrival. Added New Config: OpenAi key retrieve
    embedding.py
    graph-preprocess-simpl.py
    neo4j_import.py
    openai_key.py
    job_recommendation_system.py
    neo4j_model.py
    recommendation_generator.py
    resume_processor.py
    retrieval_engine.py
    verify.py
    view.py

--- be_repo/modules/view.py
+++ be_repo/modules/view.py
@@ -28,8 +28,8 @@ class CLIView:
         Display job recommendations to the user.
         """
         if not recommendations:
-            return "No job recommendations found based on your resume."
-        res = "\nRecommended Jobs for You:\n"
+            print("No job recommendations found based on your resume.")
+            return
+        print("\nRecommended Jobs for You:")
         for idx, job in enumerate(recommendations, start=1):
-            res += f"{idx}. {job}\n"
-        return res
+            print(f"{idx}. {job}")
